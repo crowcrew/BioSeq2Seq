6115a332f47f6e169a37c7dec34645d31f4f6897
Aly Shmahell <aly.shmahell@gmail.com>
2018-03-28 23:37:25 +0200

    fixed reocurring columns problem

--- smith_waterman_algorithm.py
+++ smith_waterman_algorithm.py
@@ -39,18 +39,12 @@ def smith_waterman():
 	global scoring_matrix
 	global gap_value
 	max_score = -1000
-	scoring_matrix = [[0]*(len(user_input)+1)]*(len(database_sequence)+1)
+	scoring_matrix = [[0 for x in range(len(user_input)+1)] for y in range(len(database_sequence)+1)]
 	for index_i, element_i in enumerate(database_sequence):
 		for index_j, element_j in enumerate(user_input):
 			substitution_value = get_substitution_value(element_i, element_j)
 			scoring_matrix[index_i+1][index_j+1] = max(scoring_matrix[index_i][index_j]+substitution_value, scoring_matrix[index_i][index_j+1]+gap_value, scoring_matrix[index_i+1][index_j]+gap_value, scoring_matrix[index_i+1][index_j+1])
 			max_score = max(max_score, scoring_matrix[index_i+1][index_j+1])
-			print scoring_matrix
-	print scoring_matrix
-	print max_score
-	print max(scoring_matrix)
-			
-				
 	
 
 if __name__ == '__main__':
@@ -62,6 +56,7 @@ if __name__ == '__main__':
 	get_substitution_value
 	print user_input
 	smith_waterman()
+	print scoring_matrix
 
 	
 
